0d19ea5b341389b92f53e37c83e956711e78b81f
lestat220255 <lestatlee@qq.com>
2023-11-13 22:36:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '16196e0c-822f-11ee-b962-0242ac120002';
+let userID = 'a1b2c3d4-e5f6-4a3b-82f5-c2d1e0f1a2b3';
 
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 
